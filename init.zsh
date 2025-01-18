6695b88777dff951b2359f33d22491d3d5a16337
aniaan <hi@aniaan.dev>
2025-01-18 13:05:09 +0800

    uddate

--- init.zsh
+++ init.zsh
@@ -1,11 +1,8 @@
-[[ -d ${HOME}/.local/bin ]] && path=(${HOME}/.local/bin ${path:#${HOME}/.local/bin})
-
 (( ${+commands[pipx]} )) && () {
   local command=${commands[pipx]}
   local compfile="$1/functions/_pipx"
 
   if [[ ! -e "$compfile" || "$compfile" -ot "$command" ]]; then
-    # 生成补全脚本
     if (( ${+commands[register-python-argcomplete]} )); then
       register-python-argcomplete pipx >| "$compfile"
       zcompile -UR "$compfile"
